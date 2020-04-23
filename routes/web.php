4140d543b21410b5b90eae709c55c6c7779a3726
Mallaury <mallaurygerard@hotmail.fr>
2020-04-23 16:42:04 +0200

    Ajout de routes (notamment) les routes 'Ajax'

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(route('appointment.index'));
 });
+
+// Ajax
+Route::get('/fetchLastname', 'PatientController@fetchLastname')->name('fetchLastname');
+Route::get('/fetchFirstname', 'PatientController@fetchFirstname')->name('fetchFirstname');
+
+// Appointments
+Route::get('/appointments', 'AppointmentController@index')->name('appointment.index');
+Route::get('/appointments/create', 'AppointmentController@create')->name('appointment.create');
+Route::get('/appointments/{appointment}', 'AppointmentController@show')->name('appointment.show');
+Route::delete('/appointments/{appointment}', 'AppointmentController@destroy')->name('appointment.destroy');
+Route::post('/appointments', 'AppointmentController@store')->name('appointment.store');
+
+// Patients
+Route::get('/patients/create', 'PatientController@create')->name('patient.create');
+Route::post('/patients', 'PatientController@store')->name('patient.store');
+Route::get('/patients/{patient}', 'PatientController@show')->name('patient.show');
